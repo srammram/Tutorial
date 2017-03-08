a45991749f52004cec688c414f8d8abf5a30d80f
ananthan1991 <ananthan@srammram.com>
2017-03-08 19:17:29 +0530

    user module all function (changepassword, forgotpassword, changeemail, changemobile, emailactive, resetpassword, smssetting)

---
+++ application/views/electtv_email_template.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Welcome To Elect TV</title>
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style=" margin:0px; padding:0px;">
+ {CONTENT}
+</body>
+</html>
