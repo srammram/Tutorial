30f84a46b20fb321add43023d30a02b93d45367c
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-03-09 19:35:26 +0530

    Modified Files comitted

--- application/modules/user/controllers/Projects.php
+++ application/modules/user/controllers/Projects.php
@@ -29,6 +29,20 @@ class Projects extends CI_Controller {
         $this->projects_table = 'projects';
         $this->project_teams_table = 'project_teams';
         $this->load->library('common');
+        $this->load->helper('download');
+    }
+
+    function _remap($method, $args) {
+
+        if (method_exists($this, $method)) {
+            $this->$method($args);
+        } else {
+            if ($method !== 'download_files') {
+                $this->index($method, $args);
+            } else if ($method == 'download_files') {
+                $this->download_files($method, $args);
+            }
+        }
     }
 
     public function index() {
@@ -201,6 +215,14 @@ class Projects extends CI_Controller {
         echo $body;
     }
 
+    public function assigned_teamprojects() {
+        $data = $this->load_module_info();
+        $user_id = $_SESSION['user_id'];
+        $getprojectdetails = $this->Mydb->custom_query("select t1.*,t2.project_name,t2.project_slug,t2.project_description from $this->project_teams_table t1 LEFT JOIN $this->projects_table t2 ON t2.id=t1.projects_id where t1.team_tl_id=$user_id and t1.status=1");
+        $data['get_assigned_project_details'] = $getprojectdetails;
+        $this->layout->display_frontend($this->folder . 'assigned_team_project', $data);
+    }
+
     public function asingnteam_byproject() {
         $project_id = $this->input->post('project_id');
         $team_duration_hours = $this->input->post('team_duration_hours');
@@ -229,6 +251,254 @@ class Projects extends CI_Controller {
             $response = "<div class='alert alert-danger'>Your details hasn't added. Please try again</div>";
         endif;
         echo $response;
+    }
+
+    function convertNumber($number) {
+        list($integer, $fraction) = explode(".", (string) $number);
+
+        $output = "";
+
+        if ($integer{0} == "-") {
+            $output = "negative ";
+            $integer = ltrim($integer, "-");
+        } else if ($integer{0} == "+") {
+            $output = "positive ";
+            $integer = ltrim($integer, "+");
+        }
+
+        if ($integer{0} == "0") {
+            $output .= "zero";
+        } else {
+            $integer = str_pad($integer, 36, "0", STR_PAD_LEFT);
+            $group = rtrim(chunk_split($integer, 3, " "), " ");
+            $groups = explode(" ", $group);
+
+            $groups2 = array();
+            foreach ($groups as $g) {
+                $groups2[] = convertThreeDigit($g{0}, $g{1}, $g{2});
+            }
+
+            for ($z = 0; $z < count($groups2); $z++) {
+                if ($groups2[$z] != "") {
+                    $output .= $groups2[$z] . convertGroup(11 - $z) . (
+                            $z < 11 && !array_search('', array_slice($groups2, $z + 1, -1)) && $groups2[11] != '' && $groups[11]{0} == '0' ? " and " : ", "
+                            );
+                }
+            }
+
+            $output = rtrim($output, ", ");
+        }
+
+        if ($fraction > 0) {
+            $output .= " point";
+            for ($i = 0; $i < strlen($fraction); $i++) {
+                $output .= " " . convertDigit($fraction{$i});
+            }
+        }
+
+        return $output;
+    }
+
+    function convertGroup($index) {
+        switch ($index) {
+            case 11:
+                return " decillion";
+            case 10:
+                return " nonillion";
+            case 9:
+                return " octillion";
+            case 8:
+                return " septillion";
+            case 7:
+                return " sextillion";
+            case 6:
+                return " quintrillion";
+            case 5:
+                return " quadrillion";
+            case 4:
+                return " trillion";
+            case 3:
+                return " billion";
+            case 2:
+                return " million";
+            case 1:
+                return " thousand";
+            case 0:
+                return "";
+        }
+    }
+
+    function convertThreeDigit($digit1, $digit2, $digit3) {
+        $buffer = "";
+
+        if ($digit1 == "0" && $digit2 == "0" && $digit3 == "0") {
+            return "";
+        }
+
+        if ($digit1 != "0") {
+            $buffer .= convertDigit($digit1) . " hundred";
+            if ($digit2 != "0" || $digit3 != "0") {
+                $buffer .= " and ";
+            }
+        }
+
+        if ($digit2 != "0") {
+            $buffer .= convertTwoDigit($digit2, $digit3);
+        } else if ($digit3 != "0") {
+            $buffer .= convertDigit($digit3);
+        }
+
+        return $buffer;
+    }
+
+    function convertTwoDigit($digit1, $digit2) {
+        if ($digit2 == "0") {
+            switch ($digit1) {
+                case "1":
+                    return "ten";
+                case "2":
+                    return "twenty";
+                case "3":
+                    return "thirty";
+                case "4":
+                    return "forty";
+                case "5":
+                    return "fifty";
+                case "6":
+                    return "sixty";
+                case "7":
+                    return "seventy";
+                case "8":
+                    return "eighty";
+                case "9":
+                    return "ninety";
+            }
+        } else if ($digit1 == "1") {
+            switch ($digit2) {
+                case "1":
+                    return "eleven";
+                case "2":
+                    return "twelve";
+                case "3":
+                    return "thirteen";
+                case "4":
+                    return "fourteen";
+                case "5":
+                    return "fifteen";
+                case "6":
+                    return "sixteen";
+                case "7":
+                    return "seventeen";
+                case "8":
+                    return "eighteen";
+                case "9":
+                    return "nineteen";
+            }
+        } else {
+            $temp = convertDigit($digit2);
+            switch ($digit1) {
+                case "2":
+                    return "twenty-$temp";
+                case "3":
+                    return "thirty-$temp";
+                case "4":
+                    return "forty-$temp";
+                case "5":
+                    return "fifty-$temp";
+                case "6":
+                    return "sixty-$temp";
+                case "7":
+                    return "seventy-$temp";
+                case "8":
+                    return "eighty-$temp";
+                case "9":
+                    return "ninety-$temp";
+            }
+        }
+    }
+
+    function convertDigit($digit) {
+        switch ($digit) {
+            case "0":
+                return "zero";
+            case "1":
+                return "one";
+            case "2":
+                return "two";
+            case "3":
+                return "three";
+            case "4":
+                return "four";
+            case "5":
+                return "five";
+            case "6":
+                return "six";
+            case "7":
+                return "seven";
+            case "8":
+                return "eight";
+            case "9":
+                return "nine";
+        }
+    }
+
+    public function view_assigned_project() {
+        $project_id = $this->input->post('project_id');
+        $getproject_team_details = $this->Mydb->custom_query("select t1.*,t3.user_name,t2.project_name,t2.project_slug,t2.project_description,t2.project_file from $this->project_teams_table t1 LEFT JOIN $this->projects_table t2 ON t2.id=t1.projects_id LEFT JOIN $this->login_table t3 ON t3.id=t1.created_by where t1.id='$project_id'");
+        $data['records'] = $getproject_team_details;
+        $body = $this->load->view($this->folder . 'asigned_view_project', $data);
+        echo $body;
+    }
+
+    public function download_files($fileName = NULL) {
+        if ($fileName[0]) {
+            $file = FCPATH . 'media/project/' . $fileName[0];
+            // check file exists    
+            if (file_exists($file)) {
+                // get file content
+                $data = file_get_contents($file);
+                //force download
+                force_download($fileName[0], $data);
+            } else {
+                // Redirect to base url
+                redirect(frontend_url() . 'projects/assigned_teamprojects');
+            }
+        }
+    }
+
+    public function downloadfiles() {
+        $fileName = $this->input->post('filename');
+        $file_path = FCPATH . 'media/project/' . $fileName;
+//        echo $file_path;
+        $this->_push_file($file_path, $fileName);
+    }
+
+    function _push_file($path, $name) {
+        $this->load->helper('download');
+        // make sure it's a file before doing anything!
+        if (is_file($path)) {
+            // required for IE
+            if (ini_get('zlib.output_compression')) {
+                ini_set('zlib.output_compression', 'Off');
+            }
+
+            // get the file mime type using the file extension
+            $this->load->helper('file');
+            $mime = get_mime_by_extension($path);
+            // Build the headers to push out the file properly.
+            header('Pragma: public');     // required
+            header('Expires: 0');         // no cache
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
+            header('Cache-Control: private', false);
+            header('Content-Type: ' . $mime);  // Add the mime type from Code igniter.
+            header('Content-Disposition: attachment; filename="' . basename($name) . '"');  // Add the file name
+            header('Content-Transfer-Encoding: binary');
+            header('Content-Length: ' . filesize($path)); // provide file size
+            header('Connection: close');
+            readfile($path); // push it out
+            exit();
+        }
     }
 
     private function load_module_info() {
