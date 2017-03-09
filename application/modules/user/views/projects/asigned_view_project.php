72d9cd5ad7c7057389e97acb3a186954c1fcf273
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-03-09 19:36:35 +0530

    Modified Files Comitted

---
+++ application/modules/user/views/projects/asigned_view_project.php
@@ -0,0 +1,29 @@
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title">Assigned Project</h4>
+</div>
+<div class="modal-body" >
+    <div class="col-xs-12" id="err_message">
+
+    </div>
+    <div class="form-group">
+        <label>Project Title</label>
+        <p style="color:#333;font-size:18px"><?php echo $records[0]['project_name']; ?></p>
+    </div>
+    <div class="form-group">
+        <label>Project Description</label>
+        <p style="color:#333;font-size:18px;word-wrap: break-word"><?php echo $records[0]['project_description']; ?></p>
+    </div>
+    <div class="form-group">
+        <label>Assigned By</label>
+        <p style="color:#333;font-size:18px;word-wrap: break-word"><?php echo $records[0]['user_name']; ?></p>
+    </div>
+    <div class="form-group">
+        <label>Project File</label>
+        <br>
+        <a href="<?php echo frontend_url() . 'projects/download_files/' . $records[0]['project_file']; ?>" style="font-size:20px" class="btn btn-success" >Download</a>
+    </div>
+</div>
+<div class="modal-footer">
+
+</div>
---
+++ application/modules/user/views/projects/assigned_team_project.php
@@ -0,0 +1,74 @@
+<?php echo $this->load->view('layout/left-menu'); ?>
+<div class="col-xs-12">
+    <h1>Assigned Projects</h1>
+    <div class="clear" style="clear: both;height:3em"></div>
+    <div class="panel panel-default">
+        <div class="panel-heading text-center">
+            Assigned Projects
+        </div>
+        <div class="panel-body">
+            <table id="assigned_projects_table" class="display" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" name="selectall" id="selectall" value="selectall"/></th>
+                        <th>S.No</th>
+                        <th>Project Title</th>
+                        <th>Project Description</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (!empty($get_assigned_project_details)):
+                        foreach ($get_assigned_project_details as $prodet):
+                            if ($prodet['status'] == 0):
+                                $lableclass = "label label-warning";
+                                $status = 'Pending';
+                            elseif ($prodet['status'] == 1):
+                                $lableclass = "label label-success";
+                                $status = 'Active';
+                            elseif ($prodet['status'] == 2):
+                                $lableclass = "label label-danger";
+                                $status = 'Ignored';
+                            elseif ($prodet['status'] == 6):
+                                $lableclass = "label label-primary";
+                                $status = 'Assigned';
+                            endif;
+                            ?>
+                            <tr>
+                                <td><input type="checkbox" class="selectthis" value="<?php echo $prodet['id']; ?>"/></td>
+                                <td><?= ($prodet['id']); ?></td>
+                                <td><?= stripslashes($prodet['project_name']); ?></td>
+                                <td><?= stripslashes($prodet['project_description']); ?></td>
+                                <td><label class="<?php echo $lableclass; ?>" style="font-size:12px"><?php echo $status; ?></label></td>
+                                <td>
+                                    <a href="#ViewAsignedProject" data-toggle="modal" class="btn btn-primary" onclick="getassignedproject_details(<?php echo $prodet['id']; ?>)"><i class="fa fa-eye"></i></a>
+                                </td>
+                            </tr>
+                            <?php
+                        endforeach;
+                    else:
+
+                    endif;
+                    ?>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+<div id="ViewAsignedProject" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content" id="viewassignedproject">
+
+        </div>
+    </div>
+</div>
+<script type="text/javascript" charset="utf-8">
+    $(document).ready(function () {
+        $('#assigned_projects_table').DataTable();
+    });
+
+</script>
