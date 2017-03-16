645d93fa242da9d01c73e8ba6b75b675f89be5d2
ananthan1991 <ananthan@srammram.com>
2017-03-16 17:39:05 +0530

    weekly report

--- application/helpers/common_settings_helper.php
+++ application/helpers/common_settings_helper.php
@@ -352,6 +352,21 @@ if (!function_exists('create_folder')) {
 	}
 
 }
+/* Color Code */
+if (!function_exists('random_color_part')) {
+
+    function random_color_part() {
+        return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
+    }
+
+}
+if (!function_exists('random_color')) {
+
+    function random_color() {
+        return random_color_part() . random_color_part() . random_color_part();
+    }
+
+}
 
 
 
