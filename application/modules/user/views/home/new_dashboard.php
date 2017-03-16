ef12931044803036476920680cebce7956a13d57
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-03-16 17:27:52 +0530

    Modified Fiels Comitted

--- application/modules/user/views/home/new_dashboard.php
+++ application/modules/user/views/home/new_dashboard.php
@@ -1,106 +1,247 @@
-
 <?php echo $this->load->view('layout/left-menu'); ?>
 
 
 <div class="clear" style="clear: both;height:3em"></div>
 <div class="common col-lg-12 col-md-6 col-md-offset-2 col-sm-12 col-xs-12">
     <div class="col-xs-offset-2  col-xs-8 text-center">
-    	<h4>Current Weelkly Report</h4>
-    	<?php $workchart = json_encode($chart_total); ?>
         <div id="chartdiv" style="height:300px"></div>
         <h5 class="text-center">Project status</h5>
     </div>
+    <?php if ($_SESSION['user_type_id'] == 1): ?>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_red one accord active" onclick="accord(1)" title="one" >
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['ongoing'] : '0'; ?></div>
+                            <div>All Ongoing Project</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
 
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_red one accord active" onclick="accord(1)" title="one" >
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-cubes fa-4x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['ongoing'] : '0'; ?></div>
-                        <div>All Ongoing Project</div>
-                    </div>
-                </div>
-            </div>
-            <a href="#">
-                <div class="panel-footer">
-                    <span class="pull-left"></span>
-                    <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
         </div>
 
-    </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_green two accord " onclick="accord(2)" title="two">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-building-o fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['Upcoming'] : '0'; ?></div>
+                            <div>All Upcoming Project</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_yellow three accord " onclick="accord(3)" title="three">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-database fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['Pipeline'] : '0'; ?></div>
+                            <div>All pipeline Project</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_blue four accord " onclick="accord(4)" title="four">
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-battery-full fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['completed'] : '0'; ?></div>
+                            <div>Completed Project</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <?php
+    elseif ($_SESSION['user_type_id'] == 4):
+        ?>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red one accord active" onclick="accord(1)" title="one" >
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['ongoing'] : '0'; ?></div>
+                            <div>All Ongoing Project</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
 
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_green two accord " onclick="accord(2)" title="two">
-        <div class="panel panel-yellow">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-building-o fa-4x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['Upcoming'] : '0'; ?></div>
-                        <div>All Upcoming Project</div>
-                    </div>
-                </div>
-            </div>
-            <a href="#">
-                <div class="panel-footer">
-                    <span class="pull-left"></span>
-                    <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
         </div>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_yellow three accord " onclick="accord(3)" title="three">
-        <div class="panel panel-red">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-database fa-4x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['Pipeline'] : '0'; ?></div>
-                        <div>All pipeline Project</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red five accord" onclick="accord(5)" title="five" >
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($assigned_count) != '0' ? $assigned_count[0]['assigned_count'] : '0'; ?></div>
+                            <div>Assigned Tasks</div>
+                        </div>
                     </div>
                 </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
             </div>
-            <a href="#">
-                <div class="panel-footer">
-                    <span class="pull-left"></span>
-                    <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
+
         </div>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 circle_blue four accord " onclick="accord(4)" title="four">
-        <div class="panel panel-green">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <i class="fa fa-battery-full fa-4x"></i>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div class="huge"><?php echo count($dashboard_count) != '0' ? $dashboard_count[0]['completed'] : '0'; ?></div>
-                        <div>Completed Project</div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red six accord" onclick="accord(6)" title="six" >
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($my_tasks_count) != '0' ? $my_tasks_count[0]['my_tasks_count'] : '0'; ?></div>
+                            <div>My Tasks</div>
+                        </div>
                     </div>
                 </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
             </div>
-            <a href="#">
-                <div class="panel-footer">
-                    <span class="pull-left"></span>
-                    <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
+
         </div>
-    </div>
+    <?php elseif ($_SESSION['user_type_id'] == 5): ?>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red five accord" onclick="accord(5)" title="five" >
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($assigned_count) != '0' ? $assigned_count[0]['assigned_count'] : '0'; ?></div>
+                            <div>Assigned Tasks</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red six accord" onclick="accord(6)" title="six" >
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($my_tasks_count) != '0' ? $my_tasks_count[0]['my_tasks_count'] : '0'; ?></div>
+                            <div>My Tasks</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    <?php elseif ($_SESSION['user_type_id'] == 6): ?>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 circle_red six accord" onclick="accord(6)" title="six" >
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-cubes fa-4x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo count($my_tasks_count) != '0' ? $my_tasks_count[0]['my_tasks_count'] : '0'; ?></div>
+                            <div>My Tasks</div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"></span>
+                        <span class="pull-right"><i class="fa fa fa-arrow-circle-down"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    <?php endif; ?>
 </div>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 section_accord">
     <div class="accord_box" id="one">
@@ -390,6 +531,16 @@
             </div>-->
         </div>
     </div>
+    <div class="accord_box" id="five">
+
+
+
+    </div>
+    <div class="accord_box" id="six">
+
+
+
+    </div>
 
 </div>
 
@@ -397,47 +548,5 @@
     <!-- bar chart -->
 
 </div>
-<script>
-	var chartData = <?php echo $workchart; ?>;
-    var chart = AmCharts.makeChart("chartdiv", {
-        "type": "serial",
-        "theme": "light",
-        "marginRight": 70,
-        "dataProvider": chartData,
-        "valueAxes": [{
-                "axisAlpha": 1,
-                "position": "left",
-                "title": "Time Durations"
-            }],
-        "startDuration": 1,
-        "graphs": [{
-                "balloonText": "<b>[[hours]]: [[value]]</b><br>[[performance]]:<b>[[status]]</b>",
-                "fillColorsField": "color",
-                "fillAlphas": 0.9,
-                "lineAlpha": 0.2,
-                "type": "column",
-                "valueField": "time",
-				"valueField2" : "status",
-				"valueField3" : "status",
-            }],
-        "chartCursor": {
-            "categoryBalloonEnabled": false,
-            "cursorAlpha": 0,
-            "zoomable": false
-        },
-        "categoryField": "datevalue",
-		"categoryField2": "performance",
-		"categoryField3": "hours",
-		
-        "categoryAxis": {
-            "gridPosition": "start",
-            "labelRotation": 0
-        },
-        "export": {
-            "enabled": true
-        }
-
-    });
-</script>
 <div class="clearfix"></div>
 </div>
