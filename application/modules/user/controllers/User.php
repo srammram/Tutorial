a3abd7bcda15275ba5da20d2dbb516b484214d37
ananthan1991 <ananthan@srammram.com>
2017-03-18 11:28:15 +0530

    menus

--- application/modules/user/controllers/User.php
+++ application/modules/user/controllers/User.php
@@ -1801,9 +1801,93 @@ class USER extends CI_Controller {
 				$data = $this->load_module_info();
 				$parent_id = decode_value($method[1]);
 				$data['menurecord'] = $this->Mydb->custom_query("SELECT * FROM $this->menus_table WHERE id='".$parent_id."'");
+				$data['subrecord'] = $this->Mydb->custom_query("SELECT * FROM $this->menus_table WHERE parent_id='".$parent_id."'");
                 $this->layout->display_frontend($this->folder . 'menus-view', $data);
 				
-			}elseif($method[0] == 'submenuadd'){
+			}elseif($method[0] == 'submenusadd'){
+				$data = $this->load_module_info();
+				$parent_id = decode_value($method[1]);
+				$data['menurecord'] = $this->Mydb->custom_query("SELECT * FROM $this->menus_table WHERE id='".$parent_id."'");
+                $this->layout->display_frontend($this->folder . 'submenus-add', $data);
+				
+			}elseif ($method[0] == 'subinsert'){
+				$this->form_validation->set_rules('menus', 'Menus Name', 'required');
+                $this->form_validation->set_rules('status', 'Status', 'required');
+
+                $menus = $this->input->post('menus');
+                $status = $this->input->post('status');
+				$parent_id = $this->input->post('parent_id');
+
+                if ($this->form_validation->run($this) == TRUE) {
+                    $check = $this->Mydb->get_record('*', $this->menus_table, array('name' => $menus, 'parent_id' => $parent_id));
+                    if (!empty($check)) {
+                        $session_datas = array('pms_err' => '1', 'pms_err_message' => 'Menus is already exit. please change data');
+                        $this->session->set_userdata($session_datas);
+                        redirect(frontend_url() . 'menus/submenusadd/'.encode_value($parent_id));
+                    } else {
+                        $insert_array = array(
+                            'name' => $menus,
+                            'slug' => strtolower(preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $menus))),
+                            'status' => $status,
+							'parent_id' => $parent_id,
+                            'created_by' => get_session_value('user_id'),
+							'created_ip' => ip2long(get_ip()),
+                        );
+                        $menusdetails = $this->Mydb->insert($this->menus_table, $insert_array);
+						
+						$log_msg = 'Menus Added by '.get_session_value('user_name');
+						$log_from = get_session_value('user_id');
+						$log_to = get_session_value('user_id');
+						log_history($log_msg, $log_from, $log_to);
+
+                        if (!empty($menusdetails)) {
+                            $session_datas = array('pms_err' => '0', 'pms_err_message' => 'Menus has been successfully added.. ');
+                            $this->session->set_userdata($session_datas);
+                            redirect(frontend_url() . 'menus/submenusadd/'.encode_value($parent_id));
+                        } else {
+                            $session_datas = array('pms_err' => '1', 'pms_err_message' => 'menus cannot be added. Please try again');
+                            $this->session->set_userdata($session_datas);
+                            redirect(frontend_url() . 'menus/submenusadd/'.encode_value($parent_id));
+                        }
+                    }
+                } else {
+                    $session_datas = array('pms_err' => '1', 'pms_err_message' => validation_errors());
+                    $this->session->set_userdata($session_datas);
+                    redirect(frontend_url() . 'menus/submenusadd/'.encode_value($parent_id));
+                }
+			}elseif ($method[0] == 'subupdate'){
+				 $edit_id = $this->input->post('edit_id');
+                $menus = $this->input->post('menus');
+				$parent_id = $this->input->post('parent_id');
+                $edit_status = $this->input->post('edit_status');
+                $update_array = array('name' => $menus,
+                    'slug' => strtolower(preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $menus))),
+                    'status' => $edit_status,
+					'parent_id' => $parent_id,
+                    'created_by' => get_session_value('user_id'),
+                    'created_ip' => ip2long(get_ip()));
+
+
+                $check = $this->Mydb->get_record('*', $this->menus_table, array('name' => $menus, 'id!=' => $edit_id, 'parent_id' => $parent_id));
+				
+				$log_msg = 'Menus Edited by '.get_session_value('user_name');
+				$log_from = get_session_value('user_id');
+				$log_to = get_session_value('user_id');
+				log_history($log_msg, $log_from, $log_to);
+
+                if (!empty($check)) {
+                    $response['message'] = "<div class='alert alert-danger'>Your sub menus already exit.</div>";
+                } else {
+                    $updatedetails = $this->Mydb->update($this->menus_table, array('id' => $edit_id), $update_array);
+
+                    if ($updatedetails) {
+                        $response['message'] = "<div class='alert alert-success'>Your sub menus has been successfully updated.</div>";
+                    } else {
+                        $response['message'] = "<div class='alert alert-danger'>Your sub menus can't be  updated. please try again</div>";
+                    }
+                }
+
+                echo json_encode($response);
 				
 			}elseif ($method[0] == 'insert') {
 
@@ -1885,7 +1969,7 @@ class USER extends CI_Controller {
             }
         } else {
             $data = $this->load_module_info();
-            $getmenusdetails = $this->Mydb->custom_query("select * from $this->menus_table WHERE status!='3'");
+            $getmenusdetails = $this->Mydb->custom_query("select * from $this->menus_table WHERE parent_id='0' AND status!='3'");
             $data['records'] = $getmenusdetails;
             $this->layout->display_frontend($this->folder . 'menus', $data);
         }
@@ -1900,6 +1984,19 @@ class USER extends CI_Controller {
         $data['status'] = $getmenudetails[0]['status'];
         $data['edit_id'] = $editid;
         $body = $this->load->view($this->folder . 'editmenus', $data);
+        echo $body;
+    }
+	
+	 ########### Sub Menus Edit ... ###########
+
+    public function editsubmenus() {
+        $editid = $this->input->post('edit_id');
+        $getmenudetails = $this->Mydb->custom_query("select name, parent_id, status from $this->menus_table where id=$editid");
+        $data['menus'] = $getmenudetails[0]['name'];
+		$data['parent_id'] = $getmenudetails[0]['parent_id'];
+        $data['status'] = $getmenudetails[0]['status'];
+        $data['edit_id'] = $editid;
+        $body = $this->load->view($this->folder . 'editsubmenus', $data);
         echo $body;
     }
 
