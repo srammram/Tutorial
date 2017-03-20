7f796d3ee190159216a7da4b252d99ab6f76b0db
ananthan1991 <ananthan@srammram.com>
2017-03-20 15:29:02 +0530

    left menus section common helper

--- application/modules/user/views/layout/left-menu.php
+++ application/modules/user/views/layout/left-menu.php
@@ -16,204 +16,38 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
                 <ul class="nav navbar-nav left_menu1">
-
-                    <li class="active open">
-                        <a href="<?php echo frontend_url() . 'dashboard'; ?>">
-                            <i class="fa fa-dashboard"></i> Dashboard
-                        </a>
-                    </li>
-                    <?php
-                    if (isset($_SESSION['user_departments_id'])):
-                        $menu_id = explode(',', $_SESSION['user_access_menus_id']);
-                        $menudetails = json_decode($_SESSION['user_access_menus_details'], TRUE);
-                        $i = 0;
-                        if (!empty($menudetails['menu_details'])):
-                            foreach ($menudetails['menu_details'] as $menudet):
-                                $menu_name[] = $menudetails['menu_details'][$i]['name'];
-                                $menu_slug[] = $menudetails['menu_details'][$i]['slug'];
-                                $menu_id[] = $menudetails['menu_details'][$i]['id'];
-                                $i++;
-                            endforeach;
-                        else:
-                            $menu_name[] = '';
-                            $menu_slug[] = '';
-                            $menu_id[] = '';
-
-                        endif;
-                        ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Users', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
+					<?php
+					$main = left_menus();
+					for($i=0; $i<count($main['menus']); $i++){
+					?>
+                   <li class="">
+                   		<a href="<?php if($main['menus'][$i]['menulink'] == 'none'){ ?>javascript:void(0); <?php }else{ echo frontend_url().$main['menus'][$i]['menulink']; } ?>" <?php if(!empty($main['submenus'][$i])){ ?> class="dropdown-toggle" data-toggle="dropdown" <?php } ?>> 
+                            <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa <?php echo $main['menus'][$i]['menuicon'];  ?>"></i></span> 
+                            <?php echo $main['menus'][$i]['name']; ?> 
+                            <?php if(!empty($main['submenus'][$i])){ ?><span class="caret"></span><?php } ?>
+                        </a> 
+                        
+                        <?php if(!empty($main['submenus'][$i])){ ?>
+                        <ul class="dropdown-menu forAnimate" role="menu">
+                        	<?php
+							for($j=0; $j<count($main['submenus'][$i]); $j++){
+							?>
                             <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-user"></i></span>
-                                    Users <span class="caret"></span>
+                            	<a href="<?php echo frontend_url().$main['submenus'][$i][$j]['menulink']; ?>">
+									<?php echo $main['submenus'][$i][$j]['name']; ?>
                                 </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li class=""><a href="<?php echo frontend_url() . 'employee/add'; ?>">Add User</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'employee'; ?>">Manage Users</a></li>
-                                </ul>
-                            </li>
-
-                        <?php endif; ?>
+                             </li>
+                            <?php
+							}
+							?>
+                        </ul>
                         <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('User Type', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons">burst_mode</span>
-                                    User Type <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li class=""><a href="<?php echo frontend_url() . 'usertype/add' ?>">Add User Type</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'usertype' ?>">Manage User Type</a></li>
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Departments', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-user"></i></span>
-                                    Departments <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li class=""><a href="<?php echo frontend_url() . 'departments/add' ?>">Add Department</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'departments' ?>">Manage Departments</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Projects', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <?php if ($_SESSION['user_type_id'] != 5): ?>
-                                <li class="">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                        <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons">burst_mode</span>
-                                        Projects <span class="caret"></span>
-                                    </a>
-                                    <ul class="dropdown-menu forAnimate" role="menu">
-                                        <li class=""><a href="<?php echo frontend_url() . 'projects/add'; ?>">Add Project</a></li>
-                                        <li><a href="<?php echo frontend_url() . 'projects/' ?>">Manage Projects</a></li>
-
-                                    </ul>
-                                </li>
-                            <?php else: ?>
-                                <li class="">
-                                    <a href="<?php echo frontend_url() . 'projects/assigned_teamprojects'; ?>">
-                                        <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-product-hunt"></i></span>
-                                        Assigned Projects 
-
-                                    </a>
-                                </li>
-                            <?php endif; ?>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Tasks', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-tasks"></i></span>
-                                    Tasks <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <?php if ($_SESSION['user_type_id'] != 6): ?>
-                                        <li class=""><a href="<?php echo frontend_url() . 'tasks/asign'; ?>">Assign Task</a></li>
-                                        <li class=""><a href="<?php echo frontend_url() . 'tasks/manage_asign_task'; ?>">Manage Assigned Task</a></li>
-                                    <?php endif; ?>
-                                    <li class=""><a href="<?php echo frontend_url() . 'tasks/add_new_task'; ?>">Add Task</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'tasks/manage_new_task'; ?>">Manage Tasks</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Settings', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-cog"></i></span>
-                                    Settings <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="<?php echo frontend_url() . 'profile'; ?>">Profile Edit</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'changepassword'; ?>">Change Password</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'emailchange'; ?>">Email Change</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'mobilechange'; ?>">Mobile Change</a></li>
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Edit Profile', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-tree"></i></span>
-                                    Holidays <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="<?php echo frontend_url() . 'holidays/add'; ?>">Add Holidays</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'holidays'; ?>">Manage Holidays</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Edit Profile', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-bar-chart"></i></span>
-                                    Menus <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="<?php echo frontend_url() . 'menus/add'; ?>">Add Menus</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'menus'; ?>">Manage Menus</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Edit Profile', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-envelope"></i></span>
-                                    Email Settings <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="<?php echo frontend_url() . 'emailsetting/add'; ?>">Add Email Setting</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'emailsetting'; ?>">Manage Email Setting</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                        <?php
-                        if (isset($_SESSION['user_access_menus_id']) && (in_array('Edit Profile', $menu_name) || $_SESSION['user_departments_id'] == 9999)):
-                            ?>
-                            <li class="">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-mobile"></i></span>
-                                    SMS Settings <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="<?php echo frontend_url() . 'smssetting/add'; ?>">Add SMS Setting</a></li>
-                                    <li><a href="<?php echo frontend_url() . 'smssetting'; ?>">Manage SMS Setting</a></li>
-
-                                </ul>
-                            </li>
-                        <?php endif; ?>
-                    <?php endif; ?>
-                    <li class="">
-                        <a href="<?php echo frontend_url() . 'reporting'; ?>">
-                            <span class="menu-icon pull-right hidden-xs showopacity glyphicon material-icons"><i class="fa fa-bar-chart"></i></span>
-                            Reporting
-                        </a>
-                    </li>
+						}
+						?>
+                   </li>
+                   <?php
+					}
+				   ?>
                 </ul>
 
             </div>
